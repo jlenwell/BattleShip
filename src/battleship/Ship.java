c67b7a028858ba07ea0149db36d149c5ee3b4570
jlenwell <373000836@mvhsrop901-15.vusdnoc.vusd.k12.ca.us>
2015-10-29 09:50:48 -0700

    Added Ship class

---
+++ src/battleship/Ship.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package battleship;
+
+/**
+ *
+ * @author 373000836
+ */
+public class Ship {
+    private int size;
+    Ship(int _size)
+    {
+        size = _size;
+    }
+    public int getsize(){
+        return size;
+    }
+}
