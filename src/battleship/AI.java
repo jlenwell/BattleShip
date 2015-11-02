fb6400537b9462318c291278f90f43f71d926a48
jlenwell <jlenwell@users.noreply.github.com>
2015-11-01 19:13:01 -0800

    COMPLETELY finished fireEasy
    progress on hard, finished infrastructure, code technically works but only easy difficulty

--- src/battleship/AI.java
+++ src/battleship/AI.java
@@ -6,7 +6,7 @@ package battleship;
 
 public static class AI {
     public enum Difficulty{
-        easy,hard
+        Easy,Medium,Hard
     }
     
     Difficulty difficulty;
@@ -15,45 +15,62 @@ public static class AI {
     private int lastColumn;
     
     AI(Difficulty _hardness){
-        difficulty = _hardness;
+        difficulty = Difficulty.Easy;
         hunt = true;
-    }    
+    }
+    public void changeDifficultyTo(Difficulty _difficulty)
+    {
+        difficulty = _difficulty;
+    }
     public void fire(Ship[][] board){
-        if(difficulty==Difficulty.easy)
+        if(difficulty==Difficulty.Easy)
         {
             fireEasy(board);
         }
-        else if (difficulty == Difficulty.hard)
+        else if (difficulty == Difficulty.Medium)
         {
-            fireHard(board)
+            fireMedium(board);
+        }
+        else if (difficulty == Difficulty.Hard)
+        {
+            fireHard(board);
         }
     }
     private void fireEasy(Ship[][] board)
     {
         int row;
         int col;
+        int bugChecker = 0;
 ////////////uncomment the below lines when ship is up to date
 //        do
 //        {           
 //            row = (int)(Math.random()*board.length);
 //            col = (int)(Math.random()*board[row].length);
+//            bugChecker++;
 //        }
 //        while(!(board[row][col] == null || (board[row][col]  != null && board[row][col].getType() != Ship.Type.Miss &&
-//              !board[row][col].getHit() ) ) );
+//              !board[row][col].getHit() ))  || bugChecker >= 100);
+//        lastRow = row;
+//        lastColumn = col;
 //        if(board[row][col] == null)
 //        {
 //            board[row][col] = new Ship(Ship.Miss);
 //        }
-//        else
+//        else if( board[row][col].getType() != Ship.Type.Miss && !board[row][col].getHit)
 //        {
 //            board[row][col].Shot();
 //            hunt = false;
 //        }
     }
+    private void fireMedium(Ship[][] board)
+    {
+        fireEasy();
+    }
     private void fireHard(Ship[][] board)
     {
         int row;
         int col;
+        int bugChecker = 0;
         if(hunt)
         {
             fireEasy(board);
@@ -64,10 +81,12 @@ public static class AI {
             {
                 row = (int)(Math.random()*board.length);
                 col = (int)(Math.random()*board[row].length);
+                bugChecker++;
             }
             while(!(board[row][col] == null || (board[row][col]  != null && board[row][col].getType() != Ship.Type.Miss &&
                   !board[row][col].getHit())) && 
-                  )
+                  (col>= lastColumn-1 && col<=lastColumn+1 && row>= lastRow-1 && row<=lastRow+1) ||
+                  bugChecker >= 100)
         }
     }
 }
