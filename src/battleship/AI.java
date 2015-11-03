6a9f22e22fc005b20eaa7f5c27f55e169965483d
jlenwell <373000836@mvhsrop901-15.vusdnoc.vusd.k12.ca.us>
2015-11-03 09:50:25 -0800

    finished medium, with a minor compiler error

--- src/battleship/AI.java
+++ src/battleship/AI.java
@@ -3,6 +3,7 @@
  * and open the template in the editor.
  */
 package battleship;
+import java.util.ArrayList;
 
 public class AI {
     public enum Difficulty{
@@ -13,10 +14,12 @@ public class AI {
     private boolean hunt;
     private int lastRow;
     private int lastColumn;
+    private ArrayList<Ship> targets;
     
     AI(Difficulty _hardness){
         difficulty = Difficulty.Easy;
         hunt = true;
+        targets = new ArrayList<Ship>();
     }
     public void setDifficulty(Difficulty _difficulty)
     {
@@ -40,8 +43,9 @@ public class AI {
             fireHard(board);
         }
     }
-    private void fireEasy(Ship[][] board)
+    private boolean fireEasy(Ship[][] board)
     {
+        boolean hit = false;
         int row;
         int col;
         int bugChecker = 0;
@@ -53,7 +57,7 @@ public class AI {
             bugChecker++;
         }
         while(!(board[row][col] == null || (board[row][col]  != null && board[row][col].getType() != Ship.Type.Miss &&
-              !board[row][col].getHit() ))  || bugChecker >= 100);
+              !board[row][col].getHit() ))  || bugChecker <= 100);
         lastRow = row;
         lastColumn = col;
         if(board[row][col] == null)
@@ -64,7 +68,9 @@ public class AI {
         {
             board[row][col].shoot();
             hunt = false;
+            hit = true;
         }
+        return hit;
     }
     private void fireMedium(Ship[][] board)
     {
@@ -77,20 +83,69 @@ public class AI {
         int bugChecker = 0;
         if(hunt)
         {
-            fireEasy(board);
+            if(fireEasy(board))
+            {
+                if(!targets.contains(board[lastRow+1][lastColumn]))
+                {
+                    targets.add(board[lastRow+1][lastColumn]);
+                }
+                if(!targets.contains(board[lastRow-1][lastColumn]))
+                {
+                    targets.add(board[lastRow-1][lastColumn]);
+                }
+                if(!targets.contains(board[lastRow][lastColumn+1]))
+                {
+                    targets.add(board[lastRow][lastColumn+1]);
+                }
+                if(!targets.contains(board[lastRow][lastColumn-1]))
+                {
+                    targets.add(board[lastRow][lastColumn-1]);
+                }
+            }
         }
-//        else
-//        {
-//            do
-//            {
-//                row = (int)(Math.random()*board.length);
-//                col = (int)(Math.random()*board[row].length);
-//                bugChecker++;
-//            }
-//            while(!(board[row][col] == null || (board[row][col]  != null && board[row][col].getType() != Ship.Type.Miss &&
-//                  !board[row][col].getHit())) && 
-//                  (col>= lastColumn-1 && col<=lastColumn+1 && row>= lastRow-1 && row<=lastRow+1) ||
-//                  bugChecker >= 100)
-//        }
+        else
+        {
+            do
+            {
+                row = (int)(Math.random()*targets.size());
+                bugChecker++;
+            }
+            while(!(targets.get(row) == null ||
+                    (targets.get(row) != null && !targets.get(row).getHit() && targets.get(row).getType() != Ship.Type.Miss) ) ||
+                    bugChecker <= 100);
+            
+            if(bugChecker >= 100)
+            {
+                hunt = true;
+                if(fireEasy(board))
+                {
+                    targets = new ArrayList<Ship>();
+                    if(!targets.contains(board[lastRow+1][lastColumn]))
+                    {
+                        targets.add(board[lastRow+1][lastColumn]);
+                    }
+                    if(!targets.contains(board[lastRow-1][lastColumn]))
+                    {
+                        targets.add(board[lastRow-1][lastColumn]);
+                    }
+                    if(!targets.contains(board[lastRow][lastColumn+1]))
+                    {
+                        targets.add(board[lastRow][lastColumn+1]);
+                    }
+                    if(!targets.contains(board[lastRow][lastColumn-1]))
+                    {
+                        targets.add(board[lastRow][lastColumn-1]);
+                    }
+                }
+            }
+            else if(targets.get(row) == null)
+            {
+//                targets.get(row) = new Ship(0,Ship.Type.Miss);
+            }
+            else
+            {
+                targets.get(row).shoot();
+            }
+        }
     }
 }
