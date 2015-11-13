3b56f26cb10580c056a0fc5e9da1b3d4b030bcb8
jlenwell <373000836@mvhsrop901-15.vusdnoc.vusd.k12.ca.us>
2015-11-13 09:55:03 -0800

    updated ship

--- src/battleship/Ship.java
+++ src/battleship/Ship.java
@@ -4,25 +4,108 @@
  */
 package battleship;
 
-import java.awt.Color;
-import java.awt.Graphics2D;
 
-/**
- *
- * @author 373000836
- */
-public class Ship {
-    private int size;
-    Ship(int _size)
-    {
-        size = _size;
+
+public class Ship{
+    public enum Type{
+        AirCraftCarrier,BattleCarrier,StandardShip,Floater,Pontoon,Miss
     }
-    public int getsize(){
+    public enum Direction{
+        Up,Down,Right,Left
+    }
+    private Type type;
+    private boolean hits[];
+    private boolean sunk;
+    private int size;
+    private int spot;
+    private Direction direction;
+    private int xpos;
+    private int ypos;
+    public Ship(int _spot, Type _type,Direction _direction)
+    { 
+        direction = _direction;
+        type = _type;
+        sunk = false;
+        if(_type == Type.Miss)
+            spot = 0;
+        else
+            spot = _spot;
+        if(type == Type.AirCraftCarrier)
+            size = 5;
+        else if(type == Type.BattleCarrier)
+            size = 4;
+        else if(type == Type.Floater)
+            size = 3;
+        else if(type == Type.StandardShip)
+            size = 3;
+        else if(type == Type.Pontoon)
+            size = 2;
+        else if(type == Type.Miss)
+            size = 1;
+        hits = new boolean[size];
+    }
+    public Ship(int _spot, Type _type)
+    { 
+        type = _type;
+        sunk = false;
+        if(_type == Type.Miss)
+            spot = 0;
+        else
+            spot = _spot;
+        if(type == Type.AirCraftCarrier)
+            size = 5;
+        else if(type == Type.BattleCarrier)
+            size = 4;
+        else if(type == Type.Floater)
+            size = 3;
+        else if(type == Type.StandardShip)
+            size = 3;
+        else if(type == Type.Pontoon)
+            size = 2;
+        else if(type == Type.Miss)
+            size = 1;
+        hits = new boolean[size];
+    }
+    public void draw(int ytop, int xleft, int hieght,int width){
+        
+    }
+    public int getSize(){
         return size;
     }
-    public static void drawShip(Graphics2D g,int xleft,int xtop,int yleft,int ytop)
+    public Direction getDirection(){
+        return direction;
+    }
+    public Type getType()
     {
-        g.setColor(Color.yellow);
-        g.fillRect(xleft, xtop , yleft , ytop);
+        return(type);
+    }
+    public int getHits(){
+        int numHits=0;
+        for(int index=0;index<hits.length;index++)
+        {
+            if(hits[index] == true)
+                numHits++;
+        }
+        return numHits;
+    }
+    public boolean getHit(){
+        if(type == Type.Miss)
+            return true;
+        else
+            return hits[spot];
+    }
+    public void shoot(){
+        hits[spot]=true;
+        if(getHits() == size)
+            sunk = true;
+    }
+    public void shootBomb(){
+        hits[spot]=true;
+        if(getHits() == size)
+            sunk = true;
+    }
+    
+    public boolean getSunk(){
+        return sunk;
     }
 }
